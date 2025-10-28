4b1c7069c323e2bfb7eb1e549044072c64e30f11
Igorase <garik.sushik@ya.ru>
2025-10-28 21:37:51 +0300

    Task2

---
+++ Tyuiu.SushchikIA.Sprint3.Task2.V24.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SushchikIA.Sprint3.Task2.V24.Lib
+{
+    public class DataService : ISprint3Task2V24
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            return 0.242;
+        }
+    }
+}
