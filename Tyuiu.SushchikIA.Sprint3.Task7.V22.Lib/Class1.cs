add71227d83291499e43f3077235027902909fc5
Igorase <garik.sushik@ya.ru>
2025-10-28 22:15:52 +0300

    Task 7

--- Tyuiu.SushchikIA.Sprint3.Task7.V22.Lib/Class1.cs
+++ Tyuiu.SushchikIA.Sprint3.Task7.V22.Lib/Class1.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.SushchikIA.Sprint3.Task7.V22.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            return [1.232, 3.2];
+            return [8.04, 6.68, 4.84, 1.76, 0.45, 0.5, -0.87, -2.42, -3.88, -6.83, -8.88];
         }
     }
 }
