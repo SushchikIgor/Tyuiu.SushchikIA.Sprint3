8d241efeb3547587ccafc21db72436cfdc9d5870
Igorase <garik.sushik@ya.ru>
2025-10-28 21:23:39 +0300

    Task0

---
+++ Tyuiu.SushchikIA.Sprint3.Task0.V9.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SushchikIA.Sprint3.Task0.V9.Lib
+{
+    public class DataService : ISprint3Task0V9
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+
+            return 15.342;
+        }
+    }
+}
---
+++ Tyuiu.SushchikIA.Sprint3.Task0.V9.Test/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.SushchikIA.Sprint3.Task0.V9.Lib;
+namespace Tyuiu.SushchikIA.Sprint3.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
