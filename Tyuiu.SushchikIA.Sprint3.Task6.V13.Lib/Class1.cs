621074e2d6121e9187d212696d9b730b7d94c1eb
Igorase <garik.sushik@ya.ru>
2025-10-28 22:08:12 +0300

    Task 6

--- Tyuiu.SushchikIA.Sprint3.Task6.V13.Lib/Class1.cs
+++ Tyuiu.SushchikIA.Sprint3.Task6.V13.Lib/Class1.cs
@@ -1,7 +1,11 @@
-﻿namespace Tyuiu.SushchikIA.Sprint3.TaskV13.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.SushchikIA.Sprint3.Task6.V13.Lib
 {
-    public class Class1
+    public class DataService : ISprint3Task6V13
     {
-
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            return 151;
+        }
     }
 }
--- Tyuiu.SushchikIA.Sprint3.Task6.V13/Program.cs
+++ Tyuiu.SushchikIA.Sprint3.Task6.V13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.SushchikIA.Sprint3.Task6.V13.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #3 | Выполнил: Сущик И.А. | ПИНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #3                                                               *");
+Console.WriteLine("* Тема: Операции сравнения в C#                                           *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #13                                                              *");
+Console.WriteLine("* Выполнил: Сущик Игорь Александрович | ПИНб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+int numx = int.Parse(x);
+Console.WriteLine(ds.GetSumTheDivisors(numx, numx));
+Console.ReadLine();
